4513908574a3e3ec43b7f03910c01945ca721d93
TJuette <tilman@golbros.de>
2022-12-24 17:02:19 +0100

    (feat)GISCE-6: Finished Ch_11

--- GISC_Ch_11.lisp
+++ GISC_Ch_11.lisp
@@ -85,37 +85,133 @@
       (return elem))))
 
 ;;11.14
-;;An error like "unbound variable x", since it hasn't been initialized before e 
+;;An error like "unbound variable x", since it hasn't been initialized before e
 
-(defun do-fact (n)
-  (do ((counter n (- counter 1))
-       (fact 1 (* counter fact)))
-      ((zerop counter) fact)))
+;;11.15
+;;In this example the iteration variable for the first element will be the first element of;;the passed list two times in a row, and the loop will end before the last element of the ;;list has been checked.
 
+;;11.16
+;;DO offers the possibility of implementing an update-function for each variable.
 
-(defun do-length (list)
-  (do ((lst list (rest lst))
-       (counter 0 (+ counter 1)))
-      ((null lst) counter)))
+;;11.17
+;;(dotimes (i 5 i) (format t "~A~%" i)) returns  4, false, 5
 
-(defun do-find (elem list)
+;;11.18
+(defun do-dotimes nil
+  (do ((i 0 (+ i 1)))
+      ((= 5 i) i)
+    (format t "~A~%" i)))
+;; In 11.17 5 is the last value of i and is returned as such, just the instructions in the ;; body of the loop aren't executed any more.
+
+;;11.19
+;; No, since they are all initialized (and possibly updated) at the same time
+
+;;11.20
+;;Yes
+
+;;11.21
+(defun it-fib (n)
+  (do ((next 1 (+ current next))
+       (current 0 next)
+       (counter n (- counter 1)))
+      ((zerop counter) current)))
+
+;;11.22
+
+;;a
+(defun complement-base (b)
+  (cond ((eql b 'a) 't)
+	((eql b 't) 'a)
+	((eql b 'g) 'c)
+	((eql b 'c) 'g)))
+
+;;b
+(defun complement-strand (s)
+  (let ((complement '()))
+    (dolist (elem s (reverse complement))
+      (push (complement-base elem) complement))))
+
+;;c
+(defun make-double (s)
+  (let ((double '()))
+    (dolist (elem s (reverse double))
+      (push (list elem (complement-base elem)) double))))
+
+;;d
+(defun count-bases (lst)
+  (let ((ad 0)
+	(th 0)
+	(gu 0)
+	(cy 0))
+  (dolist (elem lst (list (list 'a ad) (list 't th) (list 'g gu) (list 'c cy)))
+    (if (listp elem)
+	(cond ((eql (first elem) 'a) (incf ad) (incf th))
+	      ((eql (first elem) 't) (incf th) (incf ad))
+	      ((eql (first elem) 'g) (incf gu) (incf cy))
+	      ((eql (first elem) 'c) (incf cy) (incf gu)))
+	(cond ((eql elem 'a) (incf ad))
+	      ((eql elem 't) (incf th))
+	      ((eql elem 'g) (incf gu))
+	      ((eql elem 'c) (incf cy)))))))
+
+;;e
+(defun prefixp (pre list)
+  (dotimes (i (length pre) t)
+    (when (not (eql (nth i pre) (nth i list)))
+      (return nil))))
+
+;;f
+(defun appearsp (sub list)
   (do ((lst list (rest lst)))
-      ((null lst) nil)
-    (when (equal elem (first lst)) (return (first lst)))))
+      ((< (length lst) (length sub)) nil)
+    (when (prefixp sub lst)
+      (return t))))
 
-(defun rec-reverse (list)
-  (labels ((rev (l container)
-	     (cond ((null l) container)
-		   (t (rev (rest l) (cons (first l) container))))))
-    (rev list '())))
+;;g
+(defun coverp (sub list)
+  (do ((lst list (nthcdr (length sub) lst)))
+      ((null lst) t)
+    (unless (prefixp sub lst)
+      (return nil))))
 
-(defun it-fact (n)
-  (let ((prod 1))
-    (dotimes (i n prod)
-      (setf prod (* prod (+ i 1))))))
+;;h
+(defun prefix (n list)
+  (do ((result '())
+       (counter (- n 1) (- counter 1)))
+      ((zerop counter) (push (nth 0 list) result) result)
+    (push (nth counter list) result)))
+	 
+;;i
+(defun kernel (strand)
+  (do* ((list strand (rest list))
+	(prefix (list (first list)) (append prefix (list (first list)))))
+       ((null list) prefix)
+    (when (coverp prefix strand)
+      (return prefix))))
+
+;;j
+(defun draw-string (cnt string)
+  (dotimes (i cnt)
+    (format t string))
+  (format t "~%"))
+
+(defun draw-bases (strand)
+  (dolist (base strand)
+    (format t "  ~A  " base))
+  (format t  "~%"))
+    
+
+(defun draw-dna (strand)
+  (draw-string (length strand) "-----")
+  (draw-string (length strand) "  !  ")
+  (draw-bases strand)
+  (draw-string (length strand) "  .  ")
+  (draw-string (length strand) "  .  ")
+  (draw-bases (complement-strand strand))  
+  (draw-string (length strand) "  !  ")
+  (draw-string (length strand) "-----"))
 
 (defun import-all nil
   (load "GISC_Ch_11.lisp")
   (load "dtrace.lisp")
   (load "sdraw.lisp"))
-
